7fa38a2b3181000dedab7172658e0221f771fe19
Ryan Bateman <rybateman@gmail.com>
2018-11-01 20:49:27 -0800

    Spacing below biographical

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,13 +35,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Ryan Bateman',
+        short_name: 'Ryan Bateman',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/favicon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
